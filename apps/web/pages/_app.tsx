ae43f869e22c19a841b52b40217984a98f4c933b
Basile <basile64.v@gmail.com>
2023-02-06 22:40:15 +0100

    fix: update app ? maybe ?

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from 'next/app';
 import { trpc } from "trpc/client";
 
-function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
