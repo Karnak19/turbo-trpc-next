9ff23f4622b364ede81281524d66745cf7fa0e00
Basile <basile64.v@gmail.com>
2023-02-11 16:54:04 +0100

    feat(web): change title

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Web() {
   const { data } = trpc.greeting.useQuery();
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Web with title changes</h1>
       <p>{data}</p>
       <Button />
       <Button />
